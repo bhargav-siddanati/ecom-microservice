4c49f938a56226edcfe30f7984e212fb99ac9853
bhargav-siddanati <bhargav77990@gmail.com>
2026-01-03 14:29:32 +0530

    Configured the Producer App

---
+++ additional/inter-service/producer/src/main/java/com/inter/communication/producer/controller/ProviderController.java
@@ -0,0 +1,22 @@
+package com.inter.communication.producer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class ProviderController {
+
+    @Value("${server.port}")
+    private String port;
+
+    private final String INSTANCE_ID = UUID.randomUUID().toString();
+
+    @GetMapping("/instance-info")
+    public String getInstace(){
+        return "Provider Instance ID: " + INSTANCE_ID + ", Port : "
+                + port;
+    }
+}
