6ef44c55dc8f1d6035a37a85e8ec59532e86ae04
bhargav-siddanati <bhargav77990@gmail.com>
2026-02-07 08:41:41 +0530

    moved the configurations to config server

---
+++ gateway-service/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -0,0 +1,33 @@
+package com.ecommerce.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator createRoutes(RouteLocatorBuilder builder){
+        /*
+        If you dont want to expose the full path to the outer world then use the url rewrite path filter.
+        i.e, Previous: http://localhost:8080/api/product
+             Now: http://localhost:8080/product
+        Before that remove the .path("/product/**")
+        just append the '.filters( f -> f.rewritePath("/product(?<segment>?.*)","/api/product${segment}"))'
+         */
+        return builder.routes()
+                .route("user-service", r -> r.path("/api/user/**")
+                        .uri("lb://USER_SERVICE"))
+                .route("order-service", r -> r.path("/api/order/**","/api/cart/**")
+                        .uri("lb://ORDER_SERVICE"))
+                .route("product-service", r -> r.path("/api/product/**")
+                        .uri("lb://PRODUCT-SERVICE"))
+                .route("eureka-service", r -> r.path("/eureka/main")
+                        .filters(f -> f.rewritePath("/eureka/main","/"))
+                        .uri("http://localhost:8761"))
+                .route("eureka-server-static", r -> r.path("/eureka/**")
+                        .uri("http://localhost:8761"))
+                .build();
+    }
+}
