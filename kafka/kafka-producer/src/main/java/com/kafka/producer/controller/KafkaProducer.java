78d6391f8a13b032f2067ffbb7c854a140ce4c5d
bhargav-siddanati <bhargav77990@gmail.com>
2026-03-29 15:29:45 +0530

    Moving configurations from application yaml to java class

--- kafka/kafka-producer/src/main/java/com/kafka/producer/controller/KafkaProducer.java
+++ kafka/kafka-producer/src/main/java/com/kafka/producer/controller/KafkaProducer.java
@@ -2,6 +2,7 @@ package com.kafka.producer.controller;
 
 import com.kafka.producer.dto.RiderLocation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class KafkaProducer {
-    //private final KafkaTemplate<String, String> kafkaTemplate;
+
+    // Keep both templates for different use cases
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
     private final KafkaTemplate<String, RiderLocation> kafkaObjectTemplate;
 
-    /*@PostMapping("/send")
-    public String sendMessage(@RequestParam String message){
-        kafkaTemplate.send("my-topic", message);
-        return "Message sent to Kafka topic is : " + message;
-    }*/
+//    @PostMapping("/send")
+//    public String sendMessage(@RequestParam String message){
+//        kafkaTemplate.send("my-topic", message);
+//        return "Message sent to Kafka topic is : " + message;
+//    }
 
     @PostMapping("/serialize")
     public String serializeObject(){
